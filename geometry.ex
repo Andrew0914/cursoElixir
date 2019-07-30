b7e4d6e511e7bcd447e54c3b7e4d310cd1f6d937
AndrewAlan <andrewalangm@gmail.com>
2019-07-30 12:37:26 -0500

    Default error
    .ex es la extension para un archivo de modulo y exs para un script

--- geometry.ex
+++ geometry.ex
@@ -11,9 +11,19 @@ defmodule Geometry do
   def area({:circle, r}) do
     r * r * 3.14
   end
+
+  def area({:triangle, b,h}) do
+    b * h / 2
+  end
+
+  def area(unknown) do
+    {:error , {:unknown_shape , unknown}}
+  end
+
 end
 # patter matching
-IO.puts Geometry.area({:rectangle , 22,4})
-IO.puts Geometry.area({:square , 20})
-IO.puts Geometry.area({:circle , 10})
-IO.puts Geometry.area({:square , 10 , 50}) # not match
+#IO.puts Geometry.area({:rectangle , 22,4})
+#IO.puts Geometry.area({:square , 20})
+#IO.puts Geometry.area({:circle , 10})
+#IO.puts Geometry.area({:square , 10 , 50}) # not match
+#IO.puts Geometry.area({:triangle , 10 , 50}) 
