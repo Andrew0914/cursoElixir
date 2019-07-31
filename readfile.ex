b1229cc917e6a0177f53425832cdaccdca7cfd1e
AndrewAlan <andrewalangm@gmail.com>
2019-07-31 10:09:16 -0500

    lorem_ipsum exercuse

---
+++ readfile.ex
@@ -0,0 +1,24 @@
+defmodule ReadFile do
+
+    def send_repeated(linea) do
+        repeated_words = %{}
+
+        Enum.each(String.split(linea , " ") , 
+            fn palabra -> 
+                repeated_words = Map.update(repeated_words , palabra , 1 , fn x -> x + 1 end)
+            end)
+     
+        send(self() , {:calc , {"PID : #{inspect(self())}" , repeated_words} })
+    end
+
+    def read_file(file) do
+        {_, contenido}  = File.read file
+        Enum.each(String.split(contenido , "\n") , 
+            fn linea -> 
+                spawn ( fn ->   send_repeated(linea) end)
+            end)
+    end
+
+end
+
+ReadFile.read_file "lorem_ipsum_10.txt"
