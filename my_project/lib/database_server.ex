cd85ede67e4c7f76589af46fd22c8dd99f03edec
AndrewAlan <andrewalangm@gmail.com>
2019-08-01 20:54:15 -0500

    Database server

---
+++ my_project/lib/database_server.ex
@@ -0,0 +1,29 @@
+defmodule DatabaseServer do
+  
+  require Logger
+
+  def start do
+    initial_state = 0
+    spawn(fn -> 
+      initial_state = 0
+      loop(initial_state)
+    end)
+  end
+
+  defp loop(state) do
+    new_state = receive do
+      {:run_query, caller, query_def} ->
+        send(caller, {:query_result, run_query(query_def)})
+        n_state = state + 1
+        Logger.info "Server #{inspect(self())} call Count #{n_state}"
+        n_state
+    end
+    loop new_state
+  end
+
+  defp run_query(query_def) do
+    :timer.sleep(2000)
+    "#{query_def} result"
+  end
+
+end
